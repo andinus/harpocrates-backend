4745e6de66d0854ae0d0f2144828d7800b3dbcb4
Andinus <andinus@nand.sh>
2023-02-12 04:26:39 +0530

    Fix admin type in mock database

--- scripts/mock-database.sql
+++ scripts/mock-database.sql
@@ -1,9 +1,9 @@
 -- password: 1234
-INSERT INTO users.account (id, email, contact, password, verified, kyc_verified, kyc_uploaded)
+INSERT INTO users.account (id, email, contact, password, verified, kyc_verified, kyc_uploaded, type)
     VALUES (
         '793fb8f3-3a76-4522-af5a-56a916729d2b', 'admin@harpocrates.unfla.me',
         '+91 XXXXX-YYYYY', '$argon2id$v=19$m=65536,t=2,p=1$3jG47ISWVMcUEgpEWe2JFQ$uWz4LhgaiAzzA8Tv7+eHm4TI6CL4qULI7K5jqmRr+dg',
-        now(), TRUE, TRUE
+        now(), TRUE, TRUE, 'admin'
     );
 
 -- password: 1234
