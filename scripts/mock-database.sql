a39e409fc37d6a1d83a24339b68e36146e304daa
Andinus <andinus@nand.sh>
2023-02-12 01:12:37 +0530

    Add mock database script

---
+++ scripts/mock-database.sql
@@ -0,0 +1,5 @@
+INSERT INTO orderbook.detail (symbol, type, quantity, price)
+    VALUES ('HARPOCRATES', 'sell', 100, 20.30),
+           ('HARPOCRATES', 'sell', 100, 20.25),
+           ('HARPOCRATES', 'sell', 200, 20.30),
+           ('HARPOCRATES', 'buy', 250, 20.25);
