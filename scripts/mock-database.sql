3291d37e5ab07425026e40064829c4ced83c9284
Andinus <andinus@nand.sh>
2023-02-12 04:06:49 +0530

    Fix mock database
    Clash in account id.

--- scripts/mock-database.sql
+++ scripts/mock-database.sql
@@ -1,7 +1,7 @@
 -- password: 1234
 INSERT INTO users.account (id, email, contact, password, verified, kyc_verified)
     VALUES (
-        'c512a2aa-a14b-486e-9be7-651e2bb44c38', 'admin@harpocrates.unfla.me',
+        '793fb8f3-3a76-4522-af5a-56a916729d2b', 'admin@harpocrates.unfla.me',
         '+91 XXXXX-YYYYY', '$argon2id$v=19$m=65536,t=2,p=1$3jG47ISWVMcUEgpEWe2JFQ$uWz4LhgaiAzzA8Tv7+eHm4TI6CL4qULI7K5jqmRr+dg',
         now(), TRUE
     );
