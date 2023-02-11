5a2cde2bd9434b427dec9f45d452273479f4faf0
Andinus <andinus@nand.sh>
2023-02-11 15:47:49 +0530

    Schema fixes

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -2,19 +2,22 @@ CREATE TYPE resource_type AS ENUM (
     'aadhar-proof', 'pan-proof'
 );
 
+DROP SCHEMA IF EXISTS users CASCADE;
+DROP SCHEMA IF EXISTS resource CASCADE;
+
 -- user schema
 CREATE SCHEMA users;
 CREATE TABLE IF NOT EXISTS users.account(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     created TIMESTAMP WITH TIME ZONE DEFAULT now(),
     email TEXT NOT NULL UNIQUE,
-    phone TEXT NOT NULL UNIQUE,
+    phone TEXT UNIQUE,
     password TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS users.verification(
     account UUID NOT NULL REFERENCES users.account,
     created TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    token UUID NOT NULL gen_random_uuid()
+    token UUID NOT NULL DEFAULT gen_random_uuid()
 );
 
 -- resource schema
