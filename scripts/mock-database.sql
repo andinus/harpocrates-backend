6ea4380bfb1332f6e495cf53ed4663435899c88e
Andinus <andinus@nand.sh>
2023-02-12 03:45:53 +0530

    Add admin account to mock database

--- scripts/mock-database.sql
+++ scripts/mock-database.sql
@@ -1,9 +1,17 @@
 -- password: 1234
 INSERT INTO users.account (id, email, contact, password, verified, kyc)
     VALUES (
-        'c512a2aa-a14b-486e-9be7-651e2bb44c38', 'harpocrates@example.net',
+        'c512a2aa-a14b-486e-9be7-651e2bb44c38', 'admin@harpocrates.unfla.me',
         '+91 XXXXX-YYYYY', '$argon2id$v=19$m=65536,t=2,p=1$3jG47ISWVMcUEgpEWe2JFQ$uWz4LhgaiAzzA8Tv7+eHm4TI6CL4qULI7K5jqmRr+dg',
         now(), TRUE
+    );
+
+-- password: 1234
+INSERT INTO users.account (id, email, contact, password, verified, kyc)
+    VALUES (
+        'c512a2aa-a14b-486e-9be7-651e2bb44c38', 'harpocrates@example.net',
+        '+91 XXXXX-YYYYY', '$argon2id$v=19$m=65536,t=2,p=1$3jG47ISWVMcUEgpEWe2JFQ$uWz4LhgaiAzzA8Tv7+eHm4TI6CL4qULI7K5jqmRr+dg',
+        now(), FALSE
     );
 
 INSERT INTO orderbook.detail (account, symbol, type, quantity, price)
